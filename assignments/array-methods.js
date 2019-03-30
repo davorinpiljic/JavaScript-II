28803bc74008f34d0ce037d0223f2e449be2b60c
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-03-29 21:14:42 -0600

    cleaned files

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -55,38 +55,22 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
-// let fullName = [];
+let fullName = [];
 // runners.forEach(function(value) {
 //   fullName.push(value.first_name + ' ' + value.last_name)
 // })
 // console.log(fullName);
-// runners.forEach((value) => {fullName.push(value.first_name + ' ' + value.last_name)})
-// console.log(fullName)
-
-
-
-
-
-
-
-
-
+runners.forEach((value) => {fullName.push(value.first_name + ' ' + value.last_name)})
+console.log(fullName)
 
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 
 
-// let allCaps = [];
-// allCaps = runners.map(value => value.first_name.toUpperCase())
-// console.log(allCaps); 
-
-
-
-
-
-
-
+let allCaps = [];
+allCaps = runners.map(value => value.first_name.toUpperCase())
+console.log(allCaps); 
 
 
 // ==== Challenge 3: Use .filter() ====
@@ -98,50 +82,37 @@ largeShirts = runners.filter(value => value.shirt_size === "L")
 console.log(largeShirts);
 
 
-
-
-
-
-
-
-
-
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 
 
 
-// let ticketPriceTotal = [];
-// ticketPriceTotal = runners.reduce(function(sum, item) {
-//   return sum + item.donation
-// }, 0)
+let ticketPriceTotal = [];
+ticketPriceTotal = runners.reduce(function(sum, item) {
+  return sum + item.donation
+}, 0)
   
 
-// console.log(ticketPriceTotal);
-
-
-
-
-
+console.log(ticketPriceTotal);
 
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-// //create new array with only the names of the sponsoring companies, listed in alphabetical order
-// let sponsors = [];
-// runners.forEach(value => {sponsors.push(value.company_name); sponsors.sort()})
-// console.log(sponsors)
+//create new array with only the names of the sponsoring companies, listed in alphabetical order
+let sponsors = [];
+runners.forEach(value => {sponsors.push(value.company_name); sponsors.sort()})
+console.log(sponsors)
 
-// // Problem 2
-// //new array with names and info for people with donations over 200
-// let coffeeMugs = [];
-// coffeeMugs = runners.filter(value => value.donation >= 200)
-// console.log(coffeeMugs)
+// Problem 2
+//new array with names and info for people with donations over 200
+let overAmount = [];
+overAmount = runners.filter(value => value.donation >= 200)
+console.log(overAmount)
 
 // Problem 3
-//return an array of .edu e-mails to waive sign-up fee for students and educators
-// let freeRun = []
-// freeRun = runners.filter(value => value.email.includes("edu"))
-// console.log(freeRun)
+// return an array of .edu e-mails to waive sign-up fee for students
+let freeRun = []
+freeRun = runners.filter(value => value.email.includes("edu"))
+console.log(freeRun)
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,27 +2,23 @@
 
 // const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-
-
-//   //Given this problem: 
+  //Given this problem: 
   
-//   function firstItem(arr, cb) {
-//     // firstItem passes the first item of the given array to the callback function.
-//   }
+  function firstItem(arr, cb) {
+    // firstItem passes the first item of the given array to the callback function.
+  }
 
-//   // Potential Solution:
+  // Potential Solution:
 
-//   // Higher order function using "cb" as the call back
-//   function firstItem(arr, cb) {
-//     return cb('hi');
-//   }
+  // Higher order function using "cb" as the call back
+  function firstItem(arr, cb) {
+    return cb('hi');
+  }
 
-//   // Function invocation 
-//   firstItem(items, function(first) {
-//     console.log(first)
-//   });
-
-
+  // Function invocation 
+  firstItem(items, function(first) {
+    console.log(first)
+  });
 
 
 
@@ -40,15 +36,11 @@ getLength(items, (something) => console.log(something))
 
 
 
-
-
-
-
-// function last(arr, cb) {
-//   let lastIndex = arr.length - 1
-//   return cb(arr[lastIndex])
-//   // last passes the last item of the array into the callback.
-// }
+function last(arr, cb) {
+  let lastIndex = arr.length - 1
+  return cb(arr[lastIndex])
+  // last passes the last item of the array into the callback.
+}
 const last = (arr, cb) => {let lastIndex = arr.length - 1; return cb(arr[lastIndex])}
 
 // last(items, function(something) {
@@ -60,28 +52,16 @@ last(items, (something) => console.log(something))
 
 
 
-
-
-
-
-
-// function sumNums(x, y, cb) {
-//   return cb(x + y)
-//   // sumNums adds two numbers (x, y) and passes the result to the callback.
-// }
+function sumNums(x, y, cb) {
+  return cb(x + y)
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
+}
 const sumNums = (x, y, cb) => cb(x + y) 
 
 // sumNums(2, 4, function(something) {
 //   console.log(something)
 // })
 sumNums(2, 4, (something) => console.log(something))
-
-
-
-
-
-
-
 
 
 
@@ -100,22 +80,16 @@ multiplyNums(2, 4, (something) => console.log(something))
 
 
 
-
-
-
-
-
-
-// function contains(item, list, cb) {
-//   for (let i = 0; i < list.length; i++) {
-//     if (list[i] === item) {
-//       return cb(true)
-//     }
-//    return cb(false)
-//   }
-//   // contains checks if an item is present inside of the given array/list.
-//   // Pass true to the callback if it is, otherwise pass false.
-// }
+function contains(item, list, cb) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      return cb(true)
+    }
+   return cb(false)
+  }
+  // contains checks if an item is present inside of the given array/list.
+  // Pass true to the callback if it is, otherwise pass false.
+}
 const contains = (item, list, cb) => {
   for (let i = 0; i < list.length; i++) {
     if (list[i] === item) {
@@ -125,34 +99,18 @@ const contains = (item, list, cb) => {
   }
 }
 
-// contains("Pencil", items, function(something) {
-//          console.log(something)
-//          })
+contains("Pencil", items, function(something) {
+         console.log(something)
+         })
 contains("Pencil", items, (something) => console.log(something))
-      
-
-
-
-
-
-
-
-
-
-
-
-
+  
 
 
 
 // /* STRETCH PROBLEM */
 
-// function removeDuplicates(array, cb) {
-//   // removeDuplicates removes all duplicate values from the given array.
-//   // Pass the duplicate free array to the callback function.
-//   // Do not mutate the original array.
-// }
 
-
-
+  // removeDuplicates removes all duplicate values from the given array.
+  // Pass the duplicate free array to the callback function.
+  // Do not mutate the original array.
 
