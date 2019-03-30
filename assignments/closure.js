d0ca9dcb7dd44d53f0a741bb761d148f9c81c40b
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-03-29 21:26:46 -0600

    closures challenge 2

--- assignments/closure.js
+++ assignments/closure.js
@@ -16,12 +16,15 @@ console.log(func())
 
 
 // ==== Challenge 2: Create a counter function ====
+// ==== Challenge 2: Create a counter function ====
+let x = 0
 const counter = () => {
+  x++
+  return x
   // Return a function that when invoked increments and returns a counter variable.
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+console.log(counter())
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
